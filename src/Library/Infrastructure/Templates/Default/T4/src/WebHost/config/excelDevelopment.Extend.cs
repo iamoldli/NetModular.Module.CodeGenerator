894c5b90cb7acee93165276bfca2abfb309dab96
Liwj <wenju1991@163.com>
2019-12-24 21:49:17 +0800

    添加excel.json模板

---
+++ src/Library/Infrastructure/Templates/Default/T4/src/WebHost/config/excelDevelopment.Extend.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using NetModular.Module.CodeGenerator.Infrastructure.Templates.Models;
+
+namespace NetModular.Module.CodeGenerator.Infrastructure.Templates.Default.T4.src.WebHost.config
+{
+    public partial class excelDevelopment : ITemplateHandler
+    {
+        private readonly TemplateBuildModel _model;
+
+        public excelDevelopment(TemplateBuildModel model)
+        {
+            _model = model;
+        }
+
+        public void Save()
+        {
+            var dir = Path.Combine(_model.RootPath, "src/WebHost/config");
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+
+            var content = TransformText();
+            var filePath = Path.Combine(dir, "excel.Development.json");
+            File.WriteAllText(filePath, content);
+        }
+    }
+}
