43db186585132a22678b5f8af7736cbc2f2c51ec
Refactoring <Refactoring.Life@outlook.com>
2020-03-25 12:25:31 +0800

    1. Cols.js生成问题，继续使用原先的判断逻辑。 已修复问题。 2. 模块选项的IModuleOptions.Copy()方法，提示更明确的信息，提示新手实现。

--- src/Library/Infrastructure/Templates/Default/T4/src/Library/Infrastructure/Options.cs
+++ src/Library/Infrastructure/Templates/Default/T4/src/Library/Infrastructure/Options.cs
@@ -61,7 +61,8 @@ namespace NetModular.Module.CodeGenerator.Infrastructure.Templates.Default.T4.sr
             #line default
             #line hidden
             this.Write("Options : IModuleOptions\r\n    {\r\n        public IModuleOptions Copy()\r\n        {\r" +
-                    "\n            throw new System.NotImplementedException();\r\n        }\r\n    }\r\n}\r\n");
+                    "\n            throw new System.NotImplementedException(\"请实现当前类的深度复制功能，并返回新的对象。\");" +
+                    "\r\n        }\r\n    }\r\n}\r\n");
             return this.GenerationEnvironment.ToString();
         }
     }
