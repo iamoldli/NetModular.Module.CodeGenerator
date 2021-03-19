70ff7ecd2f37d50aedb28a1b4bb142cc7fdbbfa4
Mumumi <hjjixx@hotmail.com>
2021-03-19 10:27:07 +0800

    feat: 前端配置调整相对路径

--- src/Library/Infrastructure/Templates/Default/T4/src/UI/App/src/config/Config.cs
+++ src/Library/Infrastructure/Templates/Default/T4/src/UI/App/src/config/Config.cs
@@ -26,7 +26,7 @@ namespace NetModular.Module.CodeGenerator.Infrastructure.Templates.Default.T4.sr
         public virtual string TransformText()
         {
             this.Write("const isDev = process.env.NODE_ENV !== \'production\'\r\n\r\nconst config = {\r\n  baseUr" +
-                    "l: \'/api/\'\r\n}\r\n\r\n// 开发模式\r\nif (isDev) {\r\n  config.baseUrl = \'http://localhost:");
+                    "l: \'../api/\'\r\n}\r\n\r\n// 开发模式\r\nif (isDev) {\r\n  config.baseUrl = \'http://localhost:");
             
             #line 10 "D:\MyProject\NetModular\NetModular.Module.CodeGenerator\src\Library\Infrastructure\Templates\Default\T4\src\UI\App\src\config\Config.tt"
             this.Write(this.ToStringHelper.ToStringWithCulture(6220 + _model.Module.No));
